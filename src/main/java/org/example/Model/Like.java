36a5e426c48da52f4b7c4908a1579cee028a5d1e
NandaMask <nandamask4@gmail.com>
2024-05-29 02:48:32 +0330

    Like class is added

--- src/main/java/org/example/Model/Like.java
+++ src/main/java/org/example/Model/Like.java
@@ -1,4 +1,29 @@
 package org.example.Model;
 
 public class Like {
+
+    private String liker;
+
+    private String liked;
+
+    public Like (String liker , String liked){
+        this.liker = liker;
+        this.liked = liked;
+    }
+
+    public String getLiker() {
+        return liker;
+    }
+
+    public String getLiked() {
+        return liked;
+    }
+
+
+    @Override
+    public String toString() {
+        return "(Like)" +
+                "{user " + this.liker +
+                 "Liked " + this.liked;
+    }
 }
