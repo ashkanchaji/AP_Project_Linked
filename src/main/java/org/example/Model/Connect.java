0c7f9168f1ad8f7c34502b48c2c56d59caa68522
NandaMask <nandamask4@gmail.com>
2024-05-30 13:03:38 +0330

    connect class is added

--- src/main/java/org/example/Model/Connect.java
+++ src/main/java/org/example/Model/Connect.java
@@ -1,4 +1,41 @@
 package org.example.Model;
 
+import org.example.Controller.Exeptions.CharacterNumberLimitException;
+
 public class Connect {
+
+    private  String sender;
+
+    private String receiver;
+
+    private String notes;
+
+    public Connect(String sender , String receiver , String notes) throws CharacterNumberLimitException {
+        this.sender = sender;
+        this.receiver = receiver;
+        if (notes.length() > 500)
+            throw  new CharacterNumberLimitException();
+        else
+            this.notes = notes;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    @Override
+    public String toString(){
+        return "(Connect)" +
+                "{" + sender + " make connection with " + notes + " }";
+    }
+
 }
+
