d10a265e9e86121b69a02d8842f02e7aa6b1fa30
Ashkan Chaji <achaji2563@gmail.com>
2024-05-30 14:22:56 +0330

    added repost and comment dao and controller

--- src/main/java/org/example/Controller/Controllers/Controller.java
+++ src/main/java/org/example/Controller/Controllers/Controller.java
@@ -1,13 +1,16 @@
 package org.example.Controller.Controllers;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.example.Controller.DAO.*;
 
 public abstract class Controller {
-    protected static final Gson gson = new Gson();
+    protected static final Gson gson = new  GsonBuilder().setPrettyPrinting().create();
     protected static final UserDAO UserDAO = new UserDAO();
     protected static final JobDAO JobDAO = new JobDAO();
     protected static final EducationDAO EducationDAO = new EducationDAO();
     protected static final ContactsInfoDAO ContactsInfoDAO = new ContactsInfoDAO();
     protected static final PostDAO PostDAO = new PostDAO();
+    protected static final CommentDAO CommentDAO = new CommentDAO();
+    protected static final RepostDAO RepostDAO = new RepostDAO();
 }
--- src/main/java/org/example/Controller/DAO/PostDAO.java
+++ src/main/java/org/example/Controller/DAO/PostDAO.java
@@ -2,13 +2,12 @@ package org.example.Controller.DAO;
 
 import org.example.Model.Post;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class PostDAO extends GenericDAO<Post> {
-    protected final String CREATE_POST_TABLE_SQL = "CREATE TABLE IF NOT EXISTS "
+public class PostDAO extends AbstractPostDAO<Post> {
+    private final String CREATE_POST_TABLE_SQL = "CREATE TABLE IF NOT EXISTS "
             + tablePath + " ("
             + "id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, "
             + "posterID VARCHAR(45), "
@@ -20,7 +19,15 @@ public class PostDAO extends GenericDAO<Post> {
             + "byte_file_path VARCHAR(1000)"
             + ")";
 
-    public PostDAO(){
+    private final String INSERT_POST_SQL = "INSERT INTO " + tablePath +
+            "(posterID, text, likes, comments_count, createdAT, repost_count, byte_file_path) " +
+            "VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+    private final String UPDATE_POST_SQL = "UPDATE " + tablePath + " SET " +
+            "text = ?, likes = ?, comments_count = ?, createdAT = ?, repost_count = ?, byte_file_path = ? " +
+            "WHERE posterID = ?";
+
+    public PostDAO() {
         super("posts");
     }
 
@@ -40,54 +47,31 @@ public class PostDAO extends GenericDAO<Post> {
         return CREATE_POST_TABLE_SQL;
     }
 
-    public void savePost (Post post) throws SQLException {
-        String query = "INSERT INTO " + tablePath +
-                "(posterID, text, likes, comments_count, createdAT, repost_count, byte_file_path) " +
-                "VALUES (?, ?, ?, ?, ?, ?, ?)";
-        saveEntity(post, query, (ps, p) -> {
-            ps.setString(1, p.getUserId());
-            ps.setString(2, p.getText());
-            ps.setInt(3, p.getLikes());
-            ps.setInt(4, p.getComments());
-            ps.setDate(5, p.getCreatedAt());
-            ps.setInt(6, p.getReposts());
-            ps.setString(7, p.getByteFilePath());
-        });
+    public void savePost(Post post) throws SQLException {
+        savePost(post, INSERT_POST_SQL);
     }
 
-    public Post getPostByEmail (String email) throws SQLException {
+    public Post getPostByEmail(String email) throws SQLException {
         String query = "SELECT * FROM " + tablePath + " WHERE posterID = ?";
-        return getEntity(query, email);
+        return getPostByEmail(email, query);
     }
 
     public ArrayList<Post> getAllPosts() throws SQLException {
         String query = "SELECT * FROM " + tablePath;
-        return getAllEntities(query);
+        return getAllPosts(query);
     }
 
-    public void updatePost (Post post) throws SQLException {
-        String query = "UPDATE " + tablePath + " SET " +
-                "text = ?, likes = ?, comments_count = ?, createdAT = ?, repost_count = ?, byte_file_path = ? " +
-                "WHERE posterID = ?";
-
-        try (PreparedStatement ps = connection.prepareStatement(query)) {
-            ps.setString(1, post.getText());
-            ps.setInt(2, post.getLikes());
-            ps.setInt(3, post.getComments());
-            ps.setDate(4, post.getCreatedAt());
-            ps.setInt(5, post.getReposts());
-            ps.setString(6, post.getByteFilePath());
-            ps.setString(7, post.getUserId());
-        }
+    public void updatePost(Post post) throws SQLException {
+        updatePost(post, UPDATE_POST_SQL);
     }
 
-    public void deletePostByEmail (String email) throws SQLException {
+    public void deletePostByEmail(String email) throws SQLException {
         String query = "DELETE FROM " + tablePath + " WHERE posterID = ?";
-        deleteEntity(query, email);
+        deletePostByEmail(email, query);
     }
 
     public void deleteAllPosts() throws SQLException {
         String query = "DELETE FROM " + tablePath;
-        deleteAllEntities(query);
+        deleteAllPosts(query);
     }
 }
