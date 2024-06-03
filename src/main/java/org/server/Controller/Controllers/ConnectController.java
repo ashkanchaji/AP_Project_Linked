1ea19083c7df52610f016e4f044dbe614b5e52a2
NandaMask <nandamask4@gmail.com>
2024-06-03 23:20:02 +0330

    connect controller is added

---
+++ src/main/java/org/server/Controller/Controllers/ConnectController.java
@@ -0,0 +1,38 @@
+package org.server.Controller.Controllers;
+
+import org.server.Model.Connect;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ConnectController extends  Controller{
+    public static String getConnect (String email) throws SQLException {
+        Connect connect = ConnectDAO.getConnectByEmail(email);
+        return connect == null ? null : gson.toJson(connect);
+    }
+
+    public static String getAllConnects () throws SQLException {
+        ArrayList<Connect> connects = ConnectDAO.getAllConnects();
+        return gson.toJson(connects);
+    }
+
+    public static void createConnect (String json) throws SQLException {
+        Connect connect = gson.fromJson(json, Connect.class);
+
+        if (UserDAO.getUserByEmail(connect.getSender()) == null) throw new SQLException("User does not exist");
+
+        if (ConnectDAO.getConnectByEmail(connect.getSender()) == null){
+            ConnectDAO.saveConnect(connect);
+        } else {
+            ConnectDAO.updateConnect(connect);
+        }
+    }
+
+    public static void deleteConnect (String email) throws SQLException {
+        ConnectDAO.deleteConnectByEmail(email);
+    }
+
+    public static void deleteAllConnect () throws SQLException {
+        ConnectDAO.deleteAllConnects();
+    }
+}
