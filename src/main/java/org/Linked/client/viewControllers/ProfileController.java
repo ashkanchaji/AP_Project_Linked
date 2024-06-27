d5de4b79749b686f71b8b9ab4492bc62eb2f4332
Ashkan Chaji <achaji2563@gmail.com>
2024-06-27 20:56:15 +0330

    added dome features like avatar to profile view

--- src/main/java/org/Linked/client/viewControllers/ProfileController.java
+++ src/main/java/org/Linked/client/viewControllers/ProfileController.java
@@ -1,36 +1,71 @@
 package org.Linked.client.viewControllers;
 
+import io.github.gleidson28.GNAvatarView;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.image.Image;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
-public class ProfileController extends AbstractViewController {
+public class ProfileController {
 
     @FXML
     private ImageView bannerImageView;
 
     @FXML
+    private HBox homeHbox;
+
+    @FXML
+    private Button notificationButton;
+
+    @FXML
+    private HBox notificationHbox;
+
+    @FXML
+    private GNAvatarView profileAvatar;
+
+    @FXML
     private BorderPane profileBP;
+
+    @FXML
+    private Button profileButton;
+
+    @FXML
+    private HBox profileHbox;
+
+    @FXML
+    private StackPane profileStackPane;
 
     @FXML
     private BorderPane rootBP;
 
     @FXML
+    private Button searchButton;
+
+    @FXML
+    private HBox searchHbox;
+
+    @FXML
+    private Button settingButton;
+
+    @FXML
+    private HBox settingHbox;
+
+    @FXML
     private VBox tabVBox;
+
+    @FXML
+    private Button button;
 
 
     @FXML
     public void initialize() {
-        // Load the image
-//        Image image = new Image("src/main/resources/Images/banner_testi.jpg");
-//        bannerImageView.setImage(image);
-
-        // Bind ImageView's fitWidth and fitHeight properties to the BorderPane's width and height
-//        tabVBox.prefWidthProperty().bind(rootBP.widthProperty());
         bannerImageView.fitWidthProperty().bind(profileBP.widthProperty());
-//        bannerImageView.fitHeightProperty().bind(profileBP.heightProperty());
+        // Bind the avatar's position relative to the BorderPane
+//        profileAvatar.layoutXProperty().bind(profileBP.widthProperty().divide(2).subtract(profileAvatar.prefWidthProperty().divide(2)));
+//        profileAvatar.layoutYProperty().bind(profileBP.heightProperty().divide(2).subtract(profileAvatar.prefHeightProperty().divide(2)));
     }
 }
-
