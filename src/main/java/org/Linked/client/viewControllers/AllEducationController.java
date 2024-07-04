65e9014fa0a99b5d1e96bfe6de9b95a6025baadf
NandaMask <nandamask4@gmail.com>
2024-07-04 16:23:45 +0330

    show all edu view done

---
+++ src/main/java/org/Linked/client/viewControllers/AllEducationController.java
@@ -0,0 +1,30 @@
+package org.Linked.client.viewControllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class AllEducationController {
+
+    @FXML
+    private Label activityDesSELAbel;
+
+    @FXML
+    private Label addInfoSELabel;
+
+    @FXML
+    private Label gradeSELabel;
+
+    @FXML
+    private Label graduationSELabel;
+
+    @FXML
+    private Label instituteNameSELabel;
+
+    @FXML
+    private Label majorSELabel;
+
+    @FXML
+    private Label registrationSELabel;
+
+}
+
